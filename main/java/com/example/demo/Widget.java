bc0cbe3131a3fbaa498f93c164253975c087ff40
Suraksha Shetty <surakshashetty@Admins-MacBook-Pro.local>
2019-03-13 10:16:36 +0530

    second commit

---
+++ main/java/com/example/demo/Widget.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+public class Widget {
+
+    private String type;
+    private int length, height;
+
+    public Widget(String type, int length, int height) {
+        this.type = type;
+        this.length = length;
+        this.height = height;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
